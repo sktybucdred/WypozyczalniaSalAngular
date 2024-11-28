8b4b5e567438c3cc5212ca4c2598cdb77cf21da2
qer24 <qer24@go2.pl>
2024-11-28 22:19:19 +0100

    Naprawione ikony

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { AppRoutes } from './app/app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(AppRoutes),
-  provideAnimations(),
+  provideAnimations(), provideAnimationsAsync(),
   ],
 }).catch((err) => console.error(err));
