930031f570321167048423efae368062232ecaae
Kacper Przeździecki <86024@student.pb.edu.pl>
2024-12-12 13:52:16 +0100

    naprawiona edycja

--- src/app/components/sala/formularz-sali/formularz-sali.component.ts
+++ src/app/components/sala/formularz-sali/formularz-sali.component.ts
@@ -34,6 +34,7 @@ export class FormularzSaliComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.fb.group({
+      id: [this.sala?.id || null],
       nazwa: [this.sala?.nazwa || '', [Validators.required, Validators.minLength(3)]],
       pojemnosc: [this.sala?.pojemnosc || null, [Validators.required, Validators.min(1)]],
       udogodnienia: [this.sala?.udogodnienia.map((u) => u.id) || []],
